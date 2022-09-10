fe3437820454aebabf1d040ccc3e2f76f2fc9a11
Nushy Gonzalez Siso <nsiso@catalystsolves.com>
2022-09-10 12:24:26 -0500

    accepting same price still as float

--- src/main/java/io/catalyte/training/sportsproducts/domains/product/ProductRepositoryImpl.java
+++ src/main/java/io/catalyte/training/sportsproducts/domains/product/ProductRepositoryImpl.java
@@ -3,6 +3,7 @@ package io.catalyte.training.sportsproducts.domains.product;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -15,6 +16,8 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
   EntityManager entityManager;
   public Float min = 0.0f;
   public Float max = 0.0f;
+  public Float maxP = 0.0f;
+  public Float minP = 0.0f;
 
   /**
    * Construct the predicates and group of predicates to be used in the query
@@ -33,26 +36,29 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
       if (field.equals("minPrice") || (field.equals("maxPrice"))) {
         if (field.equals("minPrice")) {
           min = Float.valueOf(value.get(0));
+          minP=min;
         } else {
           min = 0.0f;
-          predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"), value.get(0)));
+          predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"), Float.valueOf(value.get(0)) + 0.001F));
         }
-
         if (field.equals("maxPrice")) {
           max = Float.valueOf(value.get(0));
+          maxP=max;
         } else {
           max = 0.0f;
-          predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"), value.get(0)));
+          predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"), Float.valueOf(value.get(0)) - 0.001F));
         }
         predicates.add(criteriaBuilder.between(root.get("price"), min, max));
 
+        if (minP.compareTo(maxP) == 0){
+          predicates.add(criteriaBuilder.equal(root.get("price"), minP));
+        }
       } else {
         value.forEach((item) -> {
           predicates.add(criteriaBuilder.equal(root.get(field), item));
         });
       }
       predicateGroups.add(criteriaBuilder.or(predicates.toArray(new Predicate[0])));
-
       predicates.clear();
     });
 
